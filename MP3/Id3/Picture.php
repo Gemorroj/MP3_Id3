1c4714acf8e89388f3110081f137e77e37af7fc2
Gemorroj <wapinet@gmail.com>
2013-07-11 12:20:53 +0300

    upd license version to 3.01

--- MP3/Id3/Picture.php
+++ MP3/Id3/Picture.php
@@ -6,14 +6,14 @@
  *
  * LICENSE: This source file is subject to version 3.0 of the PHP license
  * that is available through the world-wide-web at the following URI:
- * http://www.php.net/license/3_0.txt.  If you did not receive a copy of
+ * http://www.php.net/license/3_01.txt.  If you did not receive a copy of
  * the PHP License and are unable to obtain it through the web, please
  * send a note to license@php.net so we can mail you a copy immediately.
  *
  * @category MP3
  * @package  MP3_Id3
  * @author   Gemorroj <wapinet@mail.ru>
- * @license  http://www.php.net/license/3_0.txt  PHP License 3.0
+ * @license  http://www.php.net/license/3_01.txt  PHP License 3.01
  * @link     https://github.com/Gemorroj/MP3_Id3
  */
 
