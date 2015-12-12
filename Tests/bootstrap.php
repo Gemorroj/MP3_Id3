3d5ab3ee31cba0b9130f95efb5beddb0add3f081
Gemorroj <wapinet@gmail.com>
2015-12-12 13:46:35 +0300

    install package.xml

--- Tests/bootstrap.php
+++ Tests/bootstrap.php
@@ -1,3 +1,3 @@
 <?php
 error_reporting(E_ALL & ~E_STRICT); // fix PEAR::isError
-set_include_path(get_include_path() . PATH_SEPARATOR . dirname(__FILE__) . '/../');
+//set_include_path(get_include_path() . PATH_SEPARATOR . dirname(__FILE__) . '/../');
